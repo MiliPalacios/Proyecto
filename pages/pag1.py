2cdf4f5dfe3e6c618c6bde374f588b31393fbe77
MiliPalacios <militamolita@gmail.com>
2024-04-20 18:30:14 -0500

    renew

---
+++ pages/pag1.py
@@ -0,0 +1,7 @@
+import streamlit as st
+def page():
+    st.title("Obtener IMAGENES")
+    st.write("Aquí se visualizará de mejor manera la información relativa a LOTES")
+
+if __name__=="__main__":
+    page()
---
+++ pages/pag2.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+def page():
+    st.title("Pagina en desarrollo")
+    st.write("Descripción breve")
+
+if __name__=="__main__":
+    page()
